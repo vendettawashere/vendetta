9166e334c51562d442495aa0ce94f027bd4d7085
vendettawashere <vendettawasherebaby@gmail.com>
2026-01-19 18:59:09 -0300

    Update icehubloader.lua

--- icehubloader.lua
+++ icehubloader.lua
@@ -1,1 +1,7 @@
+local supportedgames = {131623223084840}
 
+if table.find(supportedgames, game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vendettawashere/vendetta/refs/heads/main/games/" .. game.PlaceId .. ".lua"))()
+else
+    print("Game is not supported")
+end
